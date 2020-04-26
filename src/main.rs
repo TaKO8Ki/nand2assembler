ac519c7af1415dda334fb939a14f4c1115c1fd07
TaKO8Ki <takoyaki0316@gmail.com>
2020-04-27 01:59:58 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use nand2assembly::parser;
 use std::env;
-use std::io::BufRead;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,16 +11,43 @@ fn main() {
             if !node.has_more_commands() {
                 break;
             }
+
             println!("line: {}", node.now_line);
+
             match node.command_type() {
                 Some(command_type) => println!("type: {}", command_type),
                 None => println!("command_type does not exist"),
             }
+
             match node.symbol() {
-                Some(symbol) => println!("symbol: {}", symbol),
+                Some(symbol) => {
+                    let address: usize = symbol.parse().unwrap();
+                    let formatted_address = format!("{:0>1$b}", address, 16);
+                    println!("symbol: {}, address: {}", symbol, formatted_address);
+                }
                 None => println!("symbol does not exist"),
             }
+
+            match node.dest() {
+                Some(dest) => println!("dest: {}", dest),
+                None => println!("dest does not exist"),
+            }
+
+            match node.comp() {
+                Some(comp) => println!("comp {}", comp),
+                None => println!("comp does not exist"),
+            }
+
+            match node.jump() {
+                Some(jump) => println!("jump {}", jump),
+                None => println!("jump does not exist"),
+            }
+
+            println!("")
         },
         Err(err) => println!("Error: {:?}", err),
-    }
+    };
+    let hoge: usize = "100".parse().unwrap();
+    let test = format!("{:0>1$b}", hoge, 16);
+    println!("{}", test);
 }
