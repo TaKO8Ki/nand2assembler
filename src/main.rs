34d05e1b6906bcfecbb82cf1e642ae8f0457ccac
TaKO8Ki <takoyaki0316@gmail.com>
2020-04-26 19:23:33 +0900

    add cli arg

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,23 @@ use std::io::BufRead;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // let file_name = &args[1];
-    let parser = parser::parse("hosts.txt");
+    let file_name = &args[1];
+    let parser = parser::parse(file_name);
     match parser {
         Ok(mut node) => loop {
             node.advance();
             if !node.has_more_commands() {
                 break;
             }
-            println!("{}", node.now_line);
+            println!("line: {}", node.now_line);
+            match node.command_type() {
+                Some(command_type) => println!("type: {}", command_type),
+                None => (),
+            }
+            match node.symbol() {
+                Some(symbol) => println!("symbol: {}", symbol),
+                None => (),
+            }
         },
         Err(err) => println!("Error: {:?}", err),
     }
